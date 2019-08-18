b0271fdcd49897524bd1e4a59887f7a154f2f651
thomas-armena <thomasarmena@gmail.com>
2019-08-18 14:51:30 -0400

    Starting to implement layout of page

---
+++ src/components/Header/index.js
@@ -0,0 +1,15 @@
+import React, { useRef, useEffect, useState } from 'react';
+
+const Header = () => {
+
+    return (
+        <div>
+            <div className='header'>
+                Header
+            </div>
+        </div>
+        
+    )
+}
+
+export default Header;
---
+++ src/sections/Overview.js
@@ -0,0 +1,74 @@
+import React from 'react';
+
+const Overview = () => {
+    return (
+        <div>
+            Overview
+            <br/><br/>
+            Ut elitr gubergren magna et amet magna. Eos ut sed diam aliquyam clita tempor dolor dolor, <br/>
+            eirmod rebum vero ipsum aliquyam et eirmod sit ea dolor, est sit sed et diam diam sed dolore sadipscing, <br/>
+            dolor sit nonumy dolor eirmod, vero takimata labore eos justo, stet dolore erat vero labore et lorem <br/>
+            tempor ipsum, magna gubergren sanctus dolor accusam clita dolore est amet, justo no ipsum clita ipsum <br/>
+            lorem sadipscing dolores ea, dolore vero amet ut et voluptua accusam aliquyam aliquyam ipsum. Justo stet <br/>
+            invidunt at eirmod accusam dolor et sed eirmod. Et ut sadipscing eirmod takimata eirmod diam. Dolor.<br/>
+
+            <br/><br/>
+            Ut elitr gubergren magna et amet magna. Eos ut sed diam aliquyam clita tempor dolor dolor, <br/>
+            eirmod rebum vero ipsum aliquyam et eirmod sit ea dolor, est sit sed et diam diam sed dolore sadipscing, <br/>
+            dolor sit nonumy dolor eirmod, vero takimata labore eos justo, stet dolore erat vero labore et lorem <br/>
+            tempor ipsum, magna gubergren sanctus dolor accusam clita dolore est amet, justo no ipsum clita ipsum <br/>
+            lorem sadipscing dolores ea, dolore vero amet ut et voluptua accusam aliquyam aliquyam ipsum. Justo stet <br/>
+            invidunt at eirmod accusam dolor et sed eirmod. Et ut sadipscing eirmod takimata eirmod diam. Dolor.<br/>
+
+            <br/><br/>
+            Ut elitr gubergren magna et amet magna. Eos ut sed diam aliquyam clita tempor dolor dolor, <br/>
+            eirmod rebum vero ipsum aliquyam et eirmod sit ea dolor, est sit sed et diam diam sed dolore sadipscing, <br/>
+            dolor sit nonumy dolor eirmod, vero takimata labore eos justo, stet dolore erat vero labore et lorem <br/>
+            tempor ipsum, magna gubergren sanctus dolor accusam clita dolore est amet, justo no ipsum clita ipsum <br/>
+            lorem sadipscing dolores ea, dolore vero amet ut et voluptua accusam aliquyam aliquyam ipsum. Justo stet <br/>
+            invidunt at eirmod accusam dolor et sed eirmod. Et ut sadipscing eirmod takimata eirmod diam. Dolor.<br/>
+
+            <br/><br/>
+            Ut elitr gubergren magna et amet magna. Eos ut sed diam aliquyam clita tempor dolor dolor, <br/>
+            eirmod rebum vero ipsum aliquyam et eirmod sit ea dolor, est sit sed et diam diam sed dolore sadipscing, <br/>
+            dolor sit nonumy dolor eirmod, vero takimata labore eos justo, stet dolore erat vero labore et lorem <br/>
+            tempor ipsum, magna gubergren sanctus dolor accusam clita dolore est amet, justo no ipsum clita ipsum <br/>
+            lorem sadipscing dolores ea, dolore vero amet ut et voluptua accusam aliquyam aliquyam ipsum. Justo stet <br/>
+            invidunt at eirmod accusam dolor et sed eirmod. Et ut sadipscing eirmod takimata eirmod diam. Dolor.<br/>
+
+            <br/><br/>
+            Ut elitr gubergren magna et amet magna. Eos ut sed diam aliquyam clita tempor dolor dolor, <br/>
+            eirmod rebum vero ipsum aliquyam et eirmod sit ea dolor, est sit sed et diam diam sed dolore sadipscing, <br/>
+            dolor sit nonumy dolor eirmod, vero takimata labore eos justo, stet dolore erat vero labore et lorem <br/>
+            tempor ipsum, magna gubergren sanctus dolor accusam clita dolore est amet, justo no ipsum clita ipsum <br/>
+            lorem sadipscing dolores ea, dolore vero amet ut et voluptua accusam aliquyam aliquyam ipsum. Justo stet <br/>
+            invidunt at eirmod accusam dolor et sed eirmod. Et ut sadipscing eirmod takimata eirmod diam. Dolor.<br/>
+
+            <br/><br/>
+            Ut elitr gubergren magna et amet magna. Eos ut sed diam aliquyam clita tempor dolor dolor, <br/>
+            eirmod rebum vero ipsum aliquyam et eirmod sit ea dolor, est sit sed et diam diam sed dolore sadipscing, <br/>
+            dolor sit nonumy dolor eirmod, vero takimata labore eos justo, stet dolore erat vero labore et lorem <br/>
+            tempor ipsum, magna gubergren sanctus dolor accusam clita dolore est amet, justo no ipsum clita ipsum <br/>
+            lorem sadipscing dolores ea, dolore vero amet ut et voluptua accusam aliquyam aliquyam ipsum. Justo stet <br/>
+            invidunt at eirmod accusam dolor et sed eirmod. Et ut sadipscing eirmod takimata eirmod diam. Dolor.<br/>
+
+            <br/><br/>
+            Ut elitr gubergren magna et amet magna. Eos ut sed diam aliquyam clita tempor dolor dolor, <br/>
+            eirmod rebum vero ipsum aliquyam et eirmod sit ea dolor, est sit sed et diam diam sed dolore sadipscing, <br/>
+            dolor sit nonumy dolor eirmod, vero takimata labore eos justo, stet dolore erat vero labore et lorem <br/>
+            tempor ipsum, magna gubergren sanctus dolor accusam clita dolore est amet, justo no ipsum clita ipsum <br/>
+            lorem sadipscing dolores ea, dolore vero amet ut et voluptua accusam aliquyam aliquyam ipsum. Justo stet <br/>
+            invidunt at eirmod accusam dolor et sed eirmod. Et ut sadipscing eirmod takimata eirmod diam. Dolor.<br/>
+
+            <br/><br/>
+            Ut elitr gubergren magna et amet magna. Eos ut sed diam aliquyam clita tempor dolor dolor, <br/>
+            eirmod rebum vero ipsum aliquyam et eirmod sit ea dolor, est sit sed et diam diam sed dolore sadipscing, <br/>
+            dolor sit nonumy dolor eirmod, vero takimata labore eos justo, stet dolore erat vero labore et lorem <br/>
+            tempor ipsum, magna gubergren sanctus dolor accusam clita dolore est amet, justo no ipsum clita ipsum <br/>
+            lorem sadipscing dolores ea, dolore vero amet ut et voluptua accusam aliquyam aliquyam ipsum. Justo stet <br/>
+            invidunt at eirmod accusam dolor et sed eirmod. Et ut sadipscing eirmod takimata eirmod diam. Dolor.<br/>
+        </div>
+    );
+}
+
+export default Overview;
