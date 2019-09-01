e4561069d538cf819abf71545c750df48662f43a
thomas-armena <thomasarmena@gmail.com>
2019-09-01 15:17:30 -0400

    sticky nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 
 const Nav = () => {
+    const navRef = useRef(null);
 
+    const [ isSticky, setIsSticky ] = useState(false);
+
+    useEffect(()=>{
+        const sticky = navRef.current.offsetTop;
+        window.onscroll = ()=>{
+            console.log(window.pageYOffset >= sticky)
+            setIsSticky(window.pageYOffset >= sticky);
+        }
+    }, [])
+
+    const navClassName = isSticky ? 'nav sticky' : 'nav';
+    //console.log(isSticky)
 
     return (
-        <div className='nav'>
+        <div ref={navRef} className={navClassName}>
             Nav
         </div>
     )
